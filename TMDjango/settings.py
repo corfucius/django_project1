15495431b3da9e00c6bc7a6a939e1a6aaeb29388
Corey Jones <cor@cmac-a.attlocal.net>
2018-11-01 11:12:48 -0500

    second commit

--- TMDjango/settings.py
+++ TMDjango/settings.py
@@ -129,3 +129,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'TMDjango/static')
 ]
 STATIC_URL = '/static/'
+
+#Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- realtors/models.py
+++ realtors/models.py
@@ -10,5 +10,6 @@ class Realtor(models.Model):
     email = models.CharField(max_length=50)
     is_mvp = models.BooleanField(default=False)
     hire_date = models.DateTimeField(default=datetime.now, blank=True)
+    #this def makes the realtors name show up in django admin 
     def __str__(self):
         return self.name
